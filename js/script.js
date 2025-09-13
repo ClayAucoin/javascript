aa837f4a24cd0e2eee2f33b6f417d2580a9a2b60
ClayAucoin <clay0aucoin@gmail.com>
2025-09-13 09:57:27 -0500

    docs: added comments

--- js/script.js
+++ js/script.js
@@ -15,19 +15,36 @@ const MOVIES = [
     { name: 'The Hateful Eight', year: 2015 }
 ]
 
+/*
+* choose movie number to show
+*/
 const n = 6;        // # 1-8 to change item in list
 const a = MOVIES[n];
-const mDisplay = a.name +" ("+ a.year +")";
+const mDisplay = a.name + " (" + a.year + ")";
 console.log(mDisplay);
 
 $("movieDisplay").textContent = mDisplay;
 
+/*
+* list movies in li 
+*/
+function listMovies_forEach() {
+    MOVIES.forEach(movie => {
+        const li = document.createElement("li");
+        li.textContent = `${movie.name} (${movie.year})`;
+        $("movieListforEach").appendChild(li);
+    });
+}
+listMovies_forEach();
 
+/*
+* list movies in li using map()
+*/
+function listMovies_map() {
+    // Create one big string of <li> items
+    const createHTML = MOVIES.map(movie => `<li>${movie.name} (${movie.year})</li>`).join("");
 
-const list = $("movieList");
-
-MOVIES.forEach(movie => {
-    const li = document.createElement("li");
-    li.textContent = `${movie.name} (${movie.year})`;
-    list.appendChild(li);
-});
+    // Inject into the <ul>
+    $("movieListmap").innerHTML = createHTML;
+}
+listMovies_map();
